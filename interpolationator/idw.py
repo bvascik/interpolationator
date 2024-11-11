b2bcca1c23c983afea8f8c6bd40573f9c3836a1f
bvascik <bryce.vascik@firmatek.com>
2024-11-10 21:40:50 -0500

    chunk idw for memory safe operations

--- interpolationator/idw.py
+++ interpolationator/idw.py
@@ -2,27 +2,7 @@ import numpy as np
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
 
-# TODO: add custom grid_point sizing
-
-
 def get_interpolated_values(xy, values, xi, power=2):
-    """
-    Perform IDW interpolation.
-
-    Parameters:
-    xy : ndarray of shape (n, 2)
-        Known data point coordinates.
-    values : ndarray of shape (n,)
-        Values at the known data points.
-    xi : ndarray of shape (m, 2)
-        Interpolation points.
-    power : int
-        Power parameter of the IDW formula.
-
-    Returns:
-    interpolated_values : ndarray of shape (m,)
-        Interpolated values at points xi.
-    """
     dist = distance.cdist(xi, xy, 'euclidean')
     
     with np.errstate(divide='ignore'):
@@ -40,7 +20,6 @@ def get_grid_points(xy, cell_size):
     x_min, x_max = np.min(xy[:, 0]), np.max(xy[:, 0])
     y_min, y_max = np.min(xy[:, 1]), np.max(xy[:, 1])
     
-    # Calculate the number of cells based on the cell size
     num_cells_x = int(np.ceil((x_max - x_min) / cell_size))
     num_cells_y = int(np.ceil((y_max - y_min) / cell_size))
     
@@ -51,23 +30,24 @@ def get_grid_points(xy, cell_size):
     grid_points = np.column_stack((grid_x.ravel(), grid_y.ravel()))
     return grid_x, grid_points
 
-def idw_interpolation(xy, values, idw_power=2, cell_size=20, show_plot=False):
-  grid_x, grid_points = get_grid_points(xy, cell_size)
-  grid_z = get_interpolated_values(xy, values, grid_points, power=idw_power)
+def idw_interpolation(xy, values, idw_power=2, cell_size=20, chunk_size=1000, show_plot=False):
+    grid_x, grid_points = get_grid_points(xy, cell_size)
+    grid_z = np.empty(grid_points.shape[0])
 
-  # Reshape interpolated values to match the grid
-  grid_z = grid_z.reshape(grid_x.shape)
+    for i in range(0, grid_points.shape[0], chunk_size):
+        chunk = grid_points[i:i + chunk_size]
+        grid_z[i:i + chunk_size] = get_interpolated_values(xy, values, chunk, power=idw_power)
 
-  if show_plot:
-    # Determine the extent of the plot based on the input data
-    x_min, x_max = np.min(xy[:, 0]), np.max(xy[:, 0])
-    y_min, y_max = np.min(xy[:, 1]), np.max(xy[:, 1])
-    
-    # Plot the interpolated result as an image
-    plt.imshow(grid_z, extent=(x_min, x_max, y_min, y_max), origin='lower', cmap='viridis')
-    plt.colorbar(label='Interpolated value')
-    plt.title("IDW Interpolation")
-    plt.legend()
-    plt.show()
+    grid_z = grid_z.reshape(grid_x.shape)
 
-  return grid_z
+    if show_plot:
+        x_min, x_max = np.min(xy[:, 0]), np.max(xy[:, 0])
+        y_min, y_max = np.min(xy[:, 1]), np.max(xy[:, 1])
+        
+        plt.imshow(grid_z, extent=(x_min, x_max, y_min, y_max), origin='lower', cmap='viridis')
+        plt.colorbar(label='Interpolated value')
+        plt.title("IDW Interpolation")
+        plt.legend()
+        plt.show()
+
+    return grid_z
